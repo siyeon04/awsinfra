e03d40eb4dce495ed9cc9dcbb0d719325eb731ce
siyeon04 <seanna0417@gmail.com>
2026-04-30 09:38:29 +0900

    fix: S3 backend region us-east-2로 수정

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "sk5th-lsy-terraform-state-20260429"
     key            = "training/lab06/terraform.tfstate"
-    region         = "ap-northeast-2"
+    region         = "us-east-2"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
